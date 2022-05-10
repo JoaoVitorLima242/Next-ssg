002f75153fc156cafc62aa69fab54a2286986721
JoaoVitorLima242 <jlima0500@gmail.com>
2022-05-10 17:26:41 -0300

    stating with GetStaticsPaths

--- pages/members/[login].tsx
+++ pages/members/[login].tsx
@@ -1,9 +1,35 @@
+import { GetStaticPaths, GetStaticProps } from "next";
 import { useRouter } from "next/router"
+import { userInfo } from "os";
 
-export default function Member() {
+export default function Member({ user }: any) {
     const { query } = useRouter();
 
     return (
-        <h1>{query.login}</h1>
+        <div>
+            <p>{user?.name}</p>
+        </div>
     )
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+
+    return {
+        paths: [],
+        fallback: false,
+    }
+} 
+
+export const getStaticProps: GetStaticProps = async (context) => {
+
+    const {login} = context.params;
+
+    const response = await fetch(`https://api.github.com/users/${login}`);
+    const data = response.json()
+
+    return{
+        props: {
+            user: data,
+        }
+    }
 }
